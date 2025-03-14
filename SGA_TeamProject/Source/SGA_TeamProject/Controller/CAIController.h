ffe78543aeed4d5e4c0bbec4938e794023c90fd0
GoomDolRah <dksky9955@gmail.com>
2025-03-14 17:01:28 +0900

    AIcController

---
+++ SGA_TeamProject/Source/SGA_TeamProject/Controller/CAIController.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "CAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SGA_TEAMPROJECT_API ACAIController : public AAIController
+{
+	GENERATED_BODY()
+public:
+
+
+
+
+
+private:
+
+
+
+
+
+	
+};
