335517e9b2198d482c996a145f8ff32873c0ec77
laneve471 <laneve471@naver.com>
2025-03-14 15:06:29 +0900

    InvenComponent

---
+++ SGA_TeamProject/Source/SGA_TeamProject/Character/InvenComponent.cpp
@@ -0,0 +1,107 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InvenComponent.h"
+
+// Sets default values for this component's properties
+UInvenComponent::UInvenComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	_items.SetNum(9);
+}
+
+
+// Called when the game starts
+void UInvenComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UInvenComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+FCItemInfo UInvenComponent::GetItemInfo_Index(int32 index)
+{
+	if (index < 0 || index >= _items.Num())
+		return FCItemInfo();
+
+	if (_items[index] == nullptr)
+		return FCItemInfo();
+
+	return _items[index]->GetInfo();
+}
+
+void UInvenComponent::AddItem(AItem* item)
+{
+	auto target = _items.IndexOfByPredicate([](AItem* item)->bool
+		{
+			return item == nullptr;
+		});
+
+	if (target == INDEX_NONE)
+		return;
+
+	_items[target] = item;
+
+	if (_itemChangeEvent.IsBound())
+		_itemChangeEvent.Broadcast(target, item->GetInfo());
+}
+
+AItem* UInvenComponent::DropItem()
+{
+	auto target = _items.FindLastByPredicate([](AItem* item)->bool
+		{
+			return item != nullptr;
+		});
+
+	if (target == INDEX_NONE)
+		return nullptr;
+
+	AItem* dropItemInfo = _items[target];
+	_items[target] = nullptr;
+
+	if (_itemChangeEvent.IsBound())
+		_itemChangeEvent.Broadcast(target, FCItemInfo());
+
+	return dropItemInfo;
+}
+
+AItem* UInvenComponent::DropItem(int32 index)
+{
+	if (index >= _items.Num() || index < 0)
+		return nullptr;
+
+	if (_items[index] == nullptr)
+		return nullptr;
+
+	AItem* dropItemInfo = _items[index];
+	_items[index] = nullptr;
+
+	if (_itemChangeEvent.IsBound())
+		_itemChangeEvent.Broadcast(index, FCItemInfo());
+
+	return dropItemInfo;
+}
+
+bool UInvenComponent::IsFull()
+{
+	auto target = _items.IndexOfByPredicate([](AItem* item)->bool
+		{
+			return item == nullptr;
+		});
+
+	return target == INDEX_NONE;
+}
+
---
+++ SGA_TeamProject/Source/SGA_TeamProject/Character/InvenComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "../Item/Item.h"
+#include "InvenComponent.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_TwoParams(FItemChangeEvent, int32, FCItemInfo);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SGA_TEAMPROJECT_API UInvenComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UInvenComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	FCItemInfo GetItemInfo_Index(int32 index);
+
+	void AddItem(AItem* item);
+	AItem* DropItem();
+	AItem* DropItem(int32 index);
+
+	bool IsFull();
+
+	FItemChangeEvent _itemChangeEvent;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item", meta = (AllowPrivateAccess = "true"))
+	TArray<AItem*> _items;
+};
