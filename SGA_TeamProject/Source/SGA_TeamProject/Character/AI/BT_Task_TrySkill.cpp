8a22805dd20bd0cf093e564a117b79c07f8e2810
GoomDolRah <dksky9955@gmail.com>
2025-03-30 18:18:25 +0900

    skilluse

---
+++ SGA_TeamProject/Source/SGA_TeamProject/Character/AI/BT_Task_TrySkill.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BT_Task_TrySkill.h"
+#include "../../Controller/CAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "BehaviorTree/BehaviorTree.h"
+
+#include "Kismet/KismetMathLibrary.h"
+#include "../CharacterBase.h"
+
+EBTNodeResult::Type UBT_Task_TrySkill::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	auto btNodeResult = Super::ExecuteTask(OwnerComp, NodeMemory);
+
+
+	auto currentPawn = Cast<ACharacterBase>(OwnerComp.GetAIOwner()->GetPawn());
+
+
+	if (currentPawn->IsValidLowLevel() == false)
+		return EBTNodeResult::Failed;
+
+	if (currentPawn->IsAttack())
+		return EBTNodeResult::Failed;
+
+	auto target = Cast<ACharacterBase>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(FName(TEXT("Target"))));
+	if (target->IsValidLowLevel() == false)
+		return EBTNodeResult::Failed;
+
+
+
+
+
+	return btNodeResult;
+}
